713a015b457536690bf4bf4c1f4e6f420a793d6a
jmy45 <996005918@qq.com>
2022-04-01 15:03:58 +0800

    区间DP

--- advanced/cpp/320_1.cpp
+++ advanced/cpp/320_1.cpp
@@ -13,11 +13,11 @@ int main(){
     }
     long long ans=0;
     for (int i=2*n-1;i>=0;i--)
-        for (int j=i+1;j<2*n;j++){
+        for (int j=i+1;j<2*n-1;j++){
             dp[i][j]=0;
             for (int k=i;k<j;k++)
                 //E[i][k]=(i,k+1) E[k+1][j]=(k+1,j+1)
-                dp[i][j]=max(dp[i][j],dp[i][k]+dp[k+1][j]+a[i]*a[k+1]*a[j+1==2*n?0:j+1]);
+                dp[i][j]=max(dp[i][j],dp[i][k]+dp[k+1][j]+a[i]*a[k+1]*a[j+1]);
         }
     for (int i=0;i<n;i++)
         ans=max(ans,dp[i][i+n-1]);       
