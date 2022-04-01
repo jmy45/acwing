c1c0f5b296c6e35c3f165ba97f2c4f695929bc87
jmy45 <996005918@qq.com>
2022-04-01 12:59:07 +0800

    区间DP

---
+++ advanced/cpp/320_1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int a[202];
+long long dp[202][202];
+int main(){
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for (int i=n;i<=2*n-1;i++){
+        a[i]=a[i-n];
+    }
+    long long ans=0;
+    for (int i=2*n-1;i>=0;i--)
+        for (int j=i+1;j<2*n;j++){
+            dp[i][j]=0;
+            for (int k=i;k<j;k++)
+                //E[i][k]=(i,k+1) E[k+1][j]=(k+1,j+1)
+                dp[i][j]=max(dp[i][j],dp[i][k]+dp[k+1][j]+a[i]*a[k+1]*a[j+1==2*n?0:j+1]);
+        }
+    for (int i=0;i<n;i++)
+        ans=max(ans,dp[i][i+n-1]);       
+    cout<<ans<<endl;
+    return 0;
+}
