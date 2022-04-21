d9c8985ede997d83408308c8ab9f7f36984c02a6
jmy45 <996005918@qq.com>
2022-04-21 15:40:34 +0800

    斜率优化DP

---
+++ advanced/cpp/300.cpp
@@ -0,0 +1,25 @@
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+typedef long long LL;
+const int N=5010;
+int n,s;
+LL sumt[N],sumc[N];
+LL f[N];
+int main(){
+    scanf("%d%d",&n,&s);
+    for (int i=1;i<=n;i++){
+        int t,c;
+        scanf("%d%d",&t,&c);
+        sumt[i]=sumt[i-1]+t;
+        sumc[i]=sumc[i-1]+c;
+    }
+    memset(f,0x3f,sizeof(f));
+    f[0]=0;
+    for (int i=1;i<=n;i++)
+        for (int j=0;j<i;j++)
+            f[i]=min(f[i],f[j]+sumt[i]*(sumc[i]-sumc[j])+s*(sumc[n]-sumc[j]));
+    printf("%lld\n",f[n]);
+    return 0;
+}
---
+++ advanced/cpp/302.cpp
@@ -0,0 +1,34 @@
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+typedef long long LL;
+const int N=300010;
+int n,s;
+LL c[N],t[N];
+LL f[N];
+int q[N];
+int main(){
+    scanf("%d%d",&n,&s);
+    for (int i=1;i<=n;i++){
+        scanf("%lld%lld",&t[i],&c[i]);
+        t[i]+=t[i-1];
+        c[i]+=c[i-1];
+    }
+    int hh=0,tt=0;
+    q[0]=0;
+    for (int i=1;i<=n;i++){
+        int l=hh,r=tt;
+        while (l<r){
+            int mid=l+r>>1;
+            if (f[q[mid+1]]-f[q[mid]]>(t[i]+s)*(c[q[mid+1]]-c[q[mid]])) r=mid;
+            else l=mid+1;
+        }
+        int j=q[r];
+        f[i]=f[j]-(t[i]+s)*c[j]+t[i]*c[i]+s*c[n];
+        while (hh<tt&&(double)(f[q[tt]]-f[q[tt-1]])*(c[i]-c[q[tt]])>=(double)(f[i]-f[q[tt]])*(c[q[tt]]-c[q[tt-1]])) tt--;
+        q[++tt]=i;
+    }
+    printf("%lld\n",f[n]);
+    return 0;
+}
